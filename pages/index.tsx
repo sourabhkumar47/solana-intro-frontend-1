625444dc16fb5dabf25ba714121fa97586aba9e5
sourabhkumar47 <20010001030@gateway.edu.in>
2023-06-17 23:43:07 +0530

    added fun to find if account is executable

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,24 @@ import { useState } from "react";
 import styles from "../styles/Home.module.css";
 import AddressForm from "../components/AddressForm";
 import * as Web3 from "@solana/web3.js";
-import * as web3 from "@solana/web3.js";
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
+  const [isExecutable, setIsExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
     try {
       setAddress(address);
       const key = new Web3.PublicKey(address);
       const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
+
       connection.getBalance(key).then((balance) => {
         setBalance(balance / Web3.LAMPORTS_PER_SOL);
+      });
+
+      connection.getAccountInfo(key).then((info) => {
+        setIsExecutable(info?.executable ?? false);
       });
     } catch (error) {
       setAddress("");
@@ -31,6 +36,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it Executable ? : ${isExecutable ? "Yep" : "No"}`}</p>
       </header>
     </div>
   );
